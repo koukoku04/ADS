86dd3312dba1b84aa9396fd773a94ddf914d2297
batyr <batyrhan108@gmail.com>
2024-04-07 20:56:13 +0500

    1-5 task commit

--- src/Main.java
+++ src/Main.java
@@ -20,9 +20,9 @@ public class Main {
             case 3:
                 executeThirdTask(scanner);
             case 4:
-
+                executeFourthTask(scanner);
             case 5:
-
+                executeFifthTask(scanner);
             case 6:
 
             case 7:
@@ -43,32 +43,41 @@ public class Main {
         int index=0;
         System.out.println("Iterate a list");
         int[] array = new int[n];
-//        long startTime = currentTimeMillis();
-//long endTime   = currentTimeMillis();
-//System.out.println(endTime - startTime);
+
+
         for (int i=0; i<n; i++){
             array[i]=scanner.nextInt();
         }
         int min=array[0];
+        long startTime = System.currentTimeMillis();
         System.out.println(findMin(n,array,index,min));
+        long endTime = System.currentTimeMillis();
+        long time =endTime - startTime;
+        System.out.println("execution time: " + time + "ms");
     }
     public static void executeSecondTask(Scanner scanner){
         System.out.println("Write a length of list");
         int n=scanner.nextInt();
-        int index=0;
         int total=0;
         System.out.println("Iterate a list");
         int[] array = new int[n];
         for (int i=0; i<n; i++){
             array[i]=scanner.nextInt();
+        }
+        long startTime = System.currentTimeMillis();
+        for (int i=0; i<n; i++){
             total=total+array[i];
         }
+        long endTime = System.currentTimeMillis();
+        long time =endTime - startTime;
+        System.out.println("execution time: " + time + "ms");
         System.out.println(total/n);
     }
     public static void executeThirdTask(Scanner scanner){
         System.out.println("Write a number to check it on prime");
         int n=scanner.nextInt();
         int temp=0;
+        long startTime = System.currentTimeMillis();
         for (int i=2; i<=n/2;i++){
             if(n%i==0){
                 temp+=1;
@@ -76,8 +85,54 @@ public class Main {
         }
         if(temp>0){
             System.out.println(n+" is a composite number");
+            long endTime = System.currentTimeMillis();
+            long time =endTime - startTime;
+            System.out.println("execution time: " + time + "ms");
         }
-        else System.out.println(n+" is a prime number");
+        else{
+            System.out.println(n+" is a prime number");
+            long endTime = System.currentTimeMillis();
+            long time =endTime - startTime;
+            System.out.println("execution time: " + time + "ms");
+        }
+    }
+    public static void executeFourthTask(Scanner scanner){
+        System.out.println("Write a number to check find factorial");
+        int n=scanner.nextInt();
+        int total=1;
+        long startTime = System.currentTimeMillis();
+        System.out.println(findFactorial(n,total));
+        long endTime = System.currentTimeMillis();
+        long time =endTime - startTime;
+        System.out.println("execution time: " + time + "ms");
+
+    }
+    public static void executeFifthTask(Scanner scanner){
+        System.out.println("Write a number to find n-th elements in Fibonacci sequence");
+        int n=scanner.nextInt();
+        long startTime = System.currentTimeMillis();
+        int fibNumber = findFibonacciNumber(n);
+        System.out.println("Fibonacci number at position " + n + " is: " + fibNumber);
+        long endTime = System.currentTimeMillis();
+        long time =endTime - startTime;
+        System.out.println("execution time: " + time + "ms");
+    }
+    public static int findFibonacciNumber(int n) {
+        if (n <= 0) {
+            return 0;
+        } else if (n == 1) {
+            return 1;
+        } else {
+            return findFibonacciNumber(n - 1) + findFibonacciNumber(n - 2);
+        }
+    }
+    public static int findFactorial(int n,int total){
+        if(n>0){
+            total=total*n;
+            n-=1;
+            return findFactorial(n,total);
+        }
+        return total;
     }
 
     public static int findMin(int n,int[] array,int index,int min){

3a7fd9152562cc8849c631a620f1238cb6ad45ab
batyr <batyrhan108@gmail.com>
2024-04-07 21:50:06 +0500

    5-10 task commit

--- src/Main.java
+++ src/Main.java
@@ -24,15 +24,15 @@ public class Main {
             case 5:
                 executeFifthTask(scanner);
             case 6:
-
+                executeSixthTask(scanner);
             case 7:
-
+                executeSeventhTask(scanner);
             case 8:
-
+                executeEighthTask(scanner);
             case 9:
-
+                executeNinthTask(scanner);
             case 10:
-
+                executeTenthTask(scanner);
         }
 
 
@@ -116,6 +116,104 @@ public class Main {
         long endTime = System.currentTimeMillis();
         long time =endTime - startTime;
         System.out.println("execution time: " + time + "ms");
+    }
+    public static void executeSixthTask(Scanner scanner){
+        System.out.println("Write a number to find power");
+        int a=scanner.nextInt();
+        System.out.println("Write a power");
+        int n=scanner.nextInt();
+        int total=1;
+        long startTime = System.currentTimeMillis();
+        System.out.println(findPower(n,a,total));
+        long endTime = System.currentTimeMillis();
+        long time =endTime - startTime;
+        System.out.println("execution time: " + time + "ms");
+    }
+    public static void executeSeventhTask(Scanner scanner){
+        System.out.print("Enter the number of elements: ");
+        int n = scanner.nextInt();
+
+        System.out.println("Enter " + n + " numbers:");
+        long startTime = System.currentTimeMillis();
+        reverseList(n, scanner);
+        long endTime = System.currentTimeMillis();
+        long time =endTime - startTime;
+        System.out.println("execution time: " + time + "ms");
+    }
+    public static void executeEighthTask(Scanner scanner){
+        System.out.print("Enter the string for checking it on digits");
+        String s= scanner.nextLine();
+        int n=s.length();
+        long startTime = System.currentTimeMillis();
+        System.out.println("Is "+s+"consist only digigts: " +findDigit(n-1,s));
+        long endTime = System.currentTimeMillis();
+        long time =endTime - startTime;
+        System.out.println("execution time: " + time + "ms");
+
+    }
+    public static void executeNinthTask(Scanner scanner){
+        System.out.println("To find binomial coefficient enter n an k");
+        System.out.print("Enter n:");
+        int n=scanner.nextInt();
+        System.out.print("Enter k:");
+        int k=scanner.nextInt();
+        long startTime = System.currentTimeMillis();
+        System.out.println("Answer: "+binomialCoefficient(n,k));
+        long endTime = System.currentTimeMillis();
+        long time =endTime - startTime;
+        System.out.println("execution time: " + time + "ms");
+    }
+    public static void executeTenthTask(Scanner scanner){
+        System.out.println("To find GCD enter n an k");
+        System.out.print("Enter n:");
+        int n=scanner.nextInt();
+        System.out.print("Enter k:");
+        int k=scanner.nextInt();
+        long startTime = System.currentTimeMillis();
+        System.out.println("Answer: "+findGCD(n,k));
+        long endTime = System.currentTimeMillis();
+        long time =endTime - startTime;
+        System.out.println("execution time: " + time + "ms");
+    }
+    public static int findGCD(int a, int b) {
+        if (b == 0) {
+            return a;
+        } else {
+            return findGCD(b, a % b);
+        }
+    }
+    public static int binomialCoefficient(int n, int k) {
+        if (k == 0 || k == n) {
+            return 1;
+        }
+        return binomialCoefficient(n - 1, k - 1) + binomialCoefficient(n - 1, k);
+    }
+    public static Boolean findDigit(int n,String s){
+        if(n>=0){
+            if(!Character.isDigit(s.charAt(n))){
+                return false;
+            }
+            n-=1;
+            findDigit(n,s);
+        }
+        return true;
+    }
+    public static void reverseList(int n, Scanner scanner){
+        if (n==0){
+            return;
+        }
+        int number = scanner.nextInt();
+        reverseList(n-1, scanner);
+        System.out.print(number + " ");
+    }
+
+    public static int findPower(int n, int a,int total){
+        if(n>0){
+            total*=a;
+            n-=1;
+            return findPower(n,a,total);
+        }
+        return total;
     }
     public static int findFibonacciNumber(int n) {
         if (n <= 0) {
